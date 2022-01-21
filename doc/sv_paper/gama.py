dab3962f4920fbbac92381005f4461ef8ee84788
changhoonhahn <changhoon.hahn@princeton.edu>
2022-01-21 05:14:14 -0800

    updated gama to matched dr4 catalog

--- doc/sv_paper/gama.py
+++ doc/sv_paper/gama.py
@@ -2,9 +2,14 @@ import os
 import astropy.table as atable
 
 
-def get_gama():
-    flgama = '/global/cfs/cdirs/desi/target/analysis/truth/dr9.0/south/matched/ls-dr9.0-GAMA-DR3-SpecObj-match.fits'
-    fzgama = '/global/cfs/cdirs/desi/target/analysis/truth/dr9.0/south/matched/GAMA-DR3-SpecObj-match.fits'
+def get_gama(dr=4):
+    if dr == 3: 
+        flgama = '/global/cfs/cdirs/desi/target/analysis/truth/dr9.0/south/matched/ls-dr9.0-GAMA-DR3-SpecObj-match.fits'
+        fzgama = '/global/cfs/cdirs/desi/target/analysis/truth/dr9.0/south/matched/GAMA-DR3-SpecObj-match.fits'
+    elif dr == 4: 
+        flgama = '/global/cscratch1/sd/chahah/bgs-cmxsv/gama_dr4/dr9.0/south/matched/ls-dr9.0-TilingCatv46-match.fits'
+        fzgama = '/global/cscratch1/sd/chahah/bgs-cmxsv/gama_dr4/dr9.0/south/matched/TilingCatv46-match.fits'
+        
     if not os.path.isfile(flgama): 
         flgama = '/Users/chahah/data/bgs_cmxsv/sv_paper/ls-dr9.0-GAMA-DR3-SpecObj-match.fits'
         fzgama = '/Users/chahah/data/bgs_cmxsv/sv_paper/GAMA-DR3-SpecObj-match.fits'
@@ -12,20 +17,23 @@ def get_gama():
     lgama = atable.Table.read(flgama)
     zgama = atable.Table.read(fzgama)
     
-    lgama['GAMA_NAME']          = zgama['GAMA_NAME'].data
+    # columns not in the dr4 file
+    #lgama['GAMA_NAME']          = zgama['GAMA_NAME'].data
+    #lgama['GAMA_SURVEY']        = zgama['SURVEY'].data
+    
     lgama['GAMA_SPECID']        = zgama['SPECID'].data
-    lgama['GAMA_SURVEY']        = zgama['SURVEY'].data
-    lgama['GAMA_SURVEY_CODE']  = zgama['SURVEY_CODE'].data
+    lgama['GAMA_SURVEY_CODE']   = zgama['SURVEY_CODE'].data
     lgama['GAMA_Z']             = zgama['Z'].data
     lgama['GAMA_NQ']            = zgama['NQ'].data
 
     lgama.rename_column('OBJID', 'BRICK_OBJID')
     
-    return lgama['GAMA_NAME', 'GAMA_SPECID', 'GAMA_SURVEY', 'GAMA_SURVEY_CODE', 'GAMA_Z', 'GAMA_NQ', 'BRICKID', 'BRICK_OBJID']
+    #return lgama['GAMA_NAME', 'GAMA_SPECID', 'GAMA_SURVEY', 'GAMA_SURVEY_CODE', 'GAMA_Z', 'GAMA_NQ', 'BRICKID', 'BRICK_OBJID']
+    return lgama['GAMA_SPECID', 'GAMA_SURVEY_CODE', 'GAMA_Z', 'GAMA_NQ', 'BRICKID', 'BRICK_OBJID']
     
 
-def gama_match(sv_gals):
-    lgama   = get_gama()
+def gama_match(sv_gals, dr=4):
+    lgama   = get_gama(dr=dr)
     sv_gals = atable.Table(sv_gals, copy=True)
         
     return atable.join(sv_gals, lgama, keys=['BRICK_OBJID', 'BRICKID'], join_type='left')
